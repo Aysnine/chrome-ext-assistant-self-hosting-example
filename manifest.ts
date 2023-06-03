67b25f378ff658810e7e0a6417d3a7ee92e811b8
CNine <cnine229@gmail.com>
2023-06-04 04:28:17 +0800

    fix: manifest codebase config

--- manifest.ts
+++ manifest.ts
@@ -77,7 +77,7 @@ function vitePlugin(info: {
       `<?xml version="1.0" encoding="UTF-8"?>
 <gupdate xmlns="http://www.google.com/update2/response" protocol="2.0">
   <app appid="${info.appId}">
-    <updatecheck codebase="${info.hostBaseUrl}manifest.xml" version="${info.version}" />
+    <updatecheck codebase="${info.hostBaseUrl}download.crx" version="${info.version}" />
   </app>
 </gupdate>`
     );
